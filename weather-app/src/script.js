7a7bfef00211f763f1c1e206277da2bd129e8500
FranLira8 <franciscalira8@gmail.com>
2022-05-28 17:42:50 +0200

    Finished units implementation

--- weather-app/src/script.js
+++ weather-app/src/script.js
@@ -44,19 +44,25 @@ function changeToCelsius() {
   let apiKey = "047a16ee4ff9c5f736393bccc1c2faad";
   let city = document.querySelector("#city").textContent;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  celsius.classList.add("selected");
+  fahrenheit.classList.remove("selected");
+
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
+
+let celsius = document.querySelector("#celsius");
+let fahrenheit = document.querySelector("#fahrenheit");
 
 function changeToFahrenheit() {
   // let temp = document.querySelector("#currentTemp");
   let apiKey = "047a16ee4ff9c5f736393bccc1c2faad";
   let city = document.querySelector("#city").textContent;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial`;
+  fahrenheit.classList.add("selected");
+  celsius.classList.remove("selected");
+
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
-
-let celsius = document.querySelector("#celsius");
-let fahrenheit = document.querySelector("#fahrenheit");
 
 celsius.addEventListener("click", changeToCelsius);
 fahrenheit.addEventListener("click", changeToFahrenheit);
@@ -68,33 +74,50 @@ let currentCity = document.querySelector("#city");
 function showTemperature(response) {
   console.log(response);
   let icon = document.getElementById("icon");
-
+  let unitRes = GiveselectedUnit();
+  let displayUnit;
+  if (unitRes === "imperial") {
+    displayUnit = "°F";
+  } else {
+    displayUnit = "°C";
+  }
   let iconId = response.data.weather[0].icon;
   let iconUrl = `http://openweathermap.org/img/wn/${iconId}@2x.png`;
   console.log(iconId);
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#currentTemp");
-  temperatureElement.textContent = `${temperature}°C`;
+  temperatureElement.textContent = `${temperature}${displayUnit}`;
   let minTemp = Math.round(response.data.main.temp_min);
   let minTempEl = document.querySelector("#minTemp");
   icon.setAttribute("src", iconUrl);
 
-  minTempEl.innerHTML = `Min ${minTemp}°C`;
+  minTempEl.innerHTML = `Min ${minTemp}${displayUnit}`;
 
   let maxTemp = Math.round(response.data.main.temp_max);
   let maxTempEl = document.querySelector("#maxTemp");
 
-  maxTempEl.innerHTML = `Max ${maxTemp}°C`;
+  maxTempEl.innerHTML = `Max ${maxTemp}${displayUnit}`;
   let currentCityName = response.data.name;
 
   currentCity.innerHTML = currentCityName;
 }
 
+function GiveselectedUnit() {
+  let unitEl = document.querySelectorAll(".un");
+  if (unitEl[0].className.includes("selected")) {
+    return "metric";
+  } else {
+    return "imperial";
+  }
+}
+
 function displayCity(event) {
   let apiKey = "047a16ee4ff9c5f736393bccc1c2faad";
   let city = document.querySelector("#searchBox").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  let selectedUnit = GiveselectedUnit();
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${selectedUnit}`;
 
+  // console.log(unitEl[0].className);
   event.preventDefault();
   let searchBox = document.querySelector("#searchBox");
 
@@ -110,9 +133,12 @@ form.addEventListener("submit", displayCity);
 function searchLocation(position) {
   let latitud = position.coords.latitude;
   let longitud = position.coords.longitude;
-  let units = "metric";
+  let unitEl = document.querySelectorAll(".un");
+  let selectedUnit = GiveselectedUnit();
+
   let apiKey = "047a16ee4ff9c5f736393bccc1c2faad";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apiKey}&units=${units}`;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitud}&lon=${longitud}&appid=${apiKey}&units=${selectedUnit}`;
   console.log(apiUrl);
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
